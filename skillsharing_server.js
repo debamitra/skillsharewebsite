d018a0d3f77a72e4377427bfc6925df067936a64
debamitra <debamitra.mukherjee@gmail.com>
2021-05-26 21:49:48 +0530

    project commit

---
+++ skillsharing_server.js
@@ -0,0 +1,185 @@
+const {createServer} = require("http");
+const Router = require("./router");
+//const ecstatic = require("ecstatic");
+const static1 = require('node-static');
+const {readFileSync, writeFile} = require("fs");
+
+const fileName = "./talks.json";
+
+
+const router = new Router();
+const defaultHeaders = {"Content-Type": "text/plain"};
+
+class SkillShareServer {
+  constructor(talks) {
+    this.talks = talks;
+    this.version = 0;
+    this.waiting = [];
+
+    //let fileServer = ecstatic({root: "./public"});
+    let file = new static1.Server('./public');
+
+    this.server = createServer((request, response) => {
+      //console.log("request:",request);
+      let resolved = router.resolve(this, request);
+      if (resolved) {
+        resolved.catch(error => {
+          if (error.status != null) return error;
+          return {body: String(error), status: 500};
+        }).then(({body,
+                  status = 200,
+                  headers = defaultHeaders}) => {
+          response.writeHead(status, headers);
+          response.end(body);
+        });
+      } else {
+        //console.log("hi:beflore fileserver()")
+        //fileServer(request, response);
+        file.serve(request, response);
+        //console.log("hi:beflore fileserver()")
+      }
+    });
+  }
+  start(port) {
+    this.server.listen(port);
+  }
+  stop() {
+    this.server.close();
+  }
+}
+
+const talkPath = /^\/talks\/([^\/]+)$/;
+
+router.add("GET", talkPath, async (server, title) => {
+  if (title in server.talks) {
+    return {body: JSON.stringify(server.talks[title]),
+            headers: {"Content-Type": "application/json"}};
+  } else {
+    return {status: 404, body: `No talk '${title}' found`};
+  }
+});
+
+router.add("DELETE", talkPath, async (server, title) => {
+  if (title in server.talks) {
+    delete server.talks[title];
+    server.updated();
+  }
+  return {status: 204};
+});
+
+function readStream(stream) {
+  return new Promise((resolve, reject) => {
+    let data = "";
+    stream.on("error", reject);
+    stream.on("data", chunk => data += chunk.toString());
+    stream.on("end", () => resolve(data));
+  });
+}
+
+router.add("PUT", talkPath,
+           async (server, title, request) => {
+  let requestBody = await readStream(request);
+  let talk;
+  try { talk = JSON.parse(requestBody); }
+  catch (_) { return {status: 400, body: "Invalid JSON"}; }
+
+  if (!talk ||
+      typeof talk.presenter != "string" ||
+      typeof talk.summary != "string") {
+    return {status: 400, body: "Bad talk data"};
+  }
+  server.talks[title] = {title,
+                         presenter: talk.presenter,
+                         summary: talk.summary,
+                         comments: []};
+  server.updated();
+  return {status: 204};
+});
+
+router.add("POST", /^\/talks\/([^\/]+)\/comments$/,
+           async (server, title, request) => {
+  let requestBody = await readStream(request);
+  let comment;
+  try { comment = JSON.parse(requestBody); }
+  catch (_) { return {status: 400, body: "Invalid JSON"}; }
+
+  if (!comment ||
+      typeof comment.author != "string" ||
+      typeof comment.message != "string") {
+    return {status: 400, body: "Bad comment data"};
+  } else if (title in server.talks) {
+    server.talks[title].comments.push(comment);
+    server.updated();
+    return {status: 204};
+  } else {
+    return {status: 404, body: `No talk '${title}' found`};
+  }
+});
+
+SkillShareServer.prototype.talkResponse = function() {
+  let talks = [];
+  for (let title of Object.keys(this.talks)) {
+    talks.push(this.talks[title]);
+  }
+  return {
+    body: JSON.stringify(talks),
+    headers: {"Content-Type": "application/json",
+              "ETag": `"${this.version}"`,
+              "Cache-Control": "no-store"}
+  };
+};
+
+router.add("GET", /^\/talks$/, async (server, request) => {
+  let tag = /"(.*)"/.exec(request.headers["if-none-match"]);
+  let wait = /\bwait=(\d+)/.exec(request.headers["prefer"]);
+  if (!tag || tag[1] != server.version) {
+    return server.talkResponse();
+  } else if (!wait) {
+    return {status: 304};
+  } else {
+    return server.waitForChanges(Number(wait[1]));
+  }
+});
+
+SkillShareServer.prototype.waitForChanges = function(time) {
+  return new Promise(resolve => {
+    this.waiting.push(resolve);
+    setTimeout(() => {
+      if (!this.waiting.includes(resolve)) return;
+      this.waiting = this.waiting.filter(r => r != resolve);
+      resolve({status: 304});
+    }, time * 1000);
+  });
+};
+
+SkillShareServer.prototype.updated = function() {
+  this.version++;
+  let response = this.talkResponse();
+  //write to a file ./talks.json
+  
+  writeFile("talks.json", JSON.stringify(this.talks), err => {
+    if (err) console.log(`Failed to write file: ${err}`);
+    else console.log("File written.");
+  });
+  this.waiting.forEach(resolve => resolve(response));
+  this.waiting = [];
+};
+
+const seedTalks = function() {
+
+  let json;
+  try {
+    json = JSON.parse(readFileSync("talks.json", "utf8"));
+    console.log("json:",json)
+  } catch (e) {
+    json = {};
+  }
+  return Object.assign(Object.create(null), json);
+};
+
+new SkillShareServer(seedTalks()).start(8000);
+
+
+
+
+
